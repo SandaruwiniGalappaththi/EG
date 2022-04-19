7fa5dd1bcc0091e7836270f9754bc0ff40f3a221
SupunGurusinghe <supunsameeran@gmail.com>
2022-04-19 18:21:19 +0530

    Send mail with amounts by creating functions for calculate unit price

--- EG/src/BillAutomate/Email.java
+++ EG/src/BillAutomate/Email.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.Properties;
 import javax.mail.Authenticator;
 import javax.mail.Message;
@@ -33,9 +34,11 @@ public class Email {
 	} 
 	
 	
-	public static void sendMail(String recepient, String name) throws MessagingException {
+	// send mail 
+	public static void sendMail(String recepient, String name, String amount) throws MessagingException {
 		Properties properties = new Properties();
 		
+		// email configuration
 		properties.put("mail.smtp.auth", "true");
 		properties.put("mail.smtp.starttls.enable", "true");
 		properties.put("mail.smtp.host", "smtp.gmail.com");
@@ -51,19 +54,20 @@ public class Email {
 			}
 		});
 		
-		Message message = prepareMessage(session, myAccountEmail, recepient, name);
+		Message message = prepareMessage(session, myAccountEmail, recepient, name, amount);
 		
 		Transport.send(message);
 	}
 	
-
-	private static Message prepareMessage(Session session, String myAccountEmail, String recepient, String name) {		
+	
+	// preparing email message
+	private static Message prepareMessage(Session session, String myAccountEmail, String recepient, String name, String amount) {		
 		try {
 			Message message = new MimeMessage(session);
 			message.setFrom(new InternetAddress(myAccountEmail));
 			message.setRecipient(Message.RecipientType.TO, new InternetAddress(recepient));
 			message.setSubject("Electricity Bill");
-			message.setText("Hello " + name + "\nPay from here");
+			message.setText("Hello " + name + "\nYour bill amount is Rs." + amount);
 			return message;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -72,6 +76,7 @@ public class Email {
 	}
 
 
+	// send email to all
 	public String sendEmailToAll() {
 		String output = ""; 
 		try { 
@@ -90,13 +95,25 @@ public class Email {
 			ResultSet rs = stmt.executeQuery(query); 
 			
 			// iterate through the rows in the result set
-			while (rs.next()) { 
-				String AccountNo = rs.getString("accno"); 
+			while (rs.next()) {  
+				String AccNo = rs.getString("accno"); 
 				String Name = rs.getString("name"); 
 				String Email = rs.getString("email"); 
 				
+				
+				// access current date
+				LocalDate currentdate = LocalDate.now();
+				
+				Integer Year = currentdate.getYear();
+				Integer Month = currentdate.getMonthValue();
+				String currentYear = Year.toString();
+				String currentMonth = Month.toString();
+				
+				// calculate bill
+				String amount = calcUnits(currentYear, currentMonth, AccNo);
+				
 				// Send into the sendMail()
-				sendMail(Email, Name);				
+				sendMail(Email, Name, amount);				
 			} 
 			
 			con.close(); 
@@ -117,5 +134,66 @@ public class Email {
 			System.err.println(e.getMessage()); 
 		} 
 		return output; 
+	}
+	
+	
+	// calculate units
+	public String calcUnits(String year, String month, String accNo) {
+		String output = "";
+		try { 
+			Connection con = connect(); 
+			if (con == null) {
+				return "<html><head><title>Per Unit Page</title>"
+						+ "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'>"
+						+ "</head><body>"
+						+ "<div class='card'><h4 class='text-center'>Error while connecting to the database for reading.</h4></div>"
+						+ "</body></html>";
+			} 
+						 
+			String query = "select * from calcbill where year='" +year+ "' and month='" +month+ "' and accno='" + accNo + "'"; 	
+			Statement stmt = con.createStatement(); 
+			ResultSet rs = stmt.executeQuery(query); 
+			
+			Integer previousMonth;
+			String query2;
+			ResultSet rs2;
+			
+			// iterate through the rows in the result set
+			while (rs.next()) { 
+				String Reading = rs.getString("reading"); 
+				
+				//convert month to an integer
+				previousMonth = Integer.parseInt(month) - 1;
+				query2 = "select * from calcbill where year='" +year+ "' and month='" + previousMonth.toString() + "' and accno='" + accNo + "'";	 
+				rs2 = stmt.executeQuery(query2);
+				
+				while (rs2.next()) { 
+					String Reading2 = rs2.getString("reading");
+					
+					// send to calcBill(units)
+					output = calcBill(Integer.parseInt(Reading)-Integer.parseInt(Reading2));
+					return output;
+				}				
+			} 			
+			con.close(); 
+		} 
+		catch (Exception e) { 
+			output = "<html><head><title>Per Unit Page</title>"
+					+ "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'>"
+					+ "</head><body>"
+					+ "<div class='card'><h4 class='text-center'>Error while reading.</h4></div>"
+					+ "</body></html>"; 
+			System.err.println(e.getMessage()); 
+		} 
+		return output;
+	}
+	
+	
+	// calculate bill
+	public String calcBill(Integer units) {		
+		Double amount = 0.0;
+		double basic = 500.00;
+		amount = basic + units*9.0;
+		return amount.toString();
 	}
 }
