1a310384d63e96182e35f0e4c9412de43bedaff2
IT20181406 <it20181406@my.sllit.lk>
2022-04-25 23:40:43 +0530

    Fixed some errors

--- EG/src/com/jersey/loginUser/UserServices.java
+++ EG/src/com/jersey/loginUser/UserServices.java
@@ -387,7 +387,17 @@ public class UserServices {
     			AdminLoaginBean adminloginBean = objectMapper.readValue(userdata, AdminLoaginBean.class);
     			String str = AdminLogin.login(adminloginBean);
     			
-    			if(str.equals("fail")) {
+    			
+    			if(str.equals("Please Verify Your Otp Number.")) {
+    	    		
+    				return "<html><head><title>Login Here</title>"
+    						+ "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'>"
+    						+ "</head><body>"
+    						+ "<div class='card'><h4 class='text-center'>Please Verify Your Otp Number.</h4></div>"
+    						+ "</body></html>"; 
+    			}    			
+    			
+    			else if(str.equals("fail")) {
     				
     					return "<html><head><title>Admin Login</title>"
     							+ "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'>"
