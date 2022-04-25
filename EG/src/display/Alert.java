45e0f07f27da3f312d6e9dcc8525e69366367e5e
SandaruwiniGalappaththi <sandaruwinigalappaththti@gmail.com>
2022-04-25 22:58:42 +0530

    decoration completed

--- EG/src/display/Alert.java
+++ EG/src/display/Alert.java
@@ -48,7 +48,11 @@ public class Alert {
 			        Matcher matcher = pattern.matcher(email);  
 			        //if email format is incorrect 
 			        if(matcher.matches()== false) {
-			        	output = "E-mail address is not in correct format";
+			        	output =  "<html><head><title>Payment Page</title>"
+	                            + "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'>"
+	                            + "</head><body>"
+	                            + "<div class='card'><h4 class='text-center'>E-mail address is not in correct format</h4></div>"
+	                            + "</body></html>";
 				 		return output;
 			        }
 			 	
@@ -87,7 +91,11 @@ public class Alert {
 		 		return output;
 				
 			}else if(mailOk == false) {//if email not match with account number
-				output = "E-mail address mismatch";
+				output = "<html><head><title>Payment Page</title>"
+                        + "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'>"
+                        + "</head><body>"
+                        + "<div class='card'><h4 class='text-center'>E-mail address mismatch</h4></div>"
+                        + "</body></html>";
 				return output;
 				
 			} 
@@ -95,7 +103,11 @@ public class Alert {
 		 
 		 			
 		 if(accountOk == false) {//if account number is invalid
-			   output = "Account number mismatch";
+			   output = "<html><head><title>Payment Page</title>"
+                       + "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'>"
+                       + "</head><body>"
+                       + "<div class='card'><h4 class='text-center'>Account number mismatch</h4></div>"
+                       + "</body></html>";
 			   return output;
 		}
 		 			
